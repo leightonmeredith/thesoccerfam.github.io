85649a5d02e8af68a02e31e71f44eb4208c50512
luluket2000 <luluket2000@yahoo.com>
2020-04-24 14:37:24 -0400

    header/footer function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -67,6 +67,7 @@ import { TemplateComponent } from './template/template.component';
 import { FormsModule } from '@angular/forms';
 import { RightNavComponent } from './right-nav/right-nav.component';
 import { LeftNavComponent } from './left-nav/left-nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -75,7 +76,8 @@ import { LeftNavComponent } from './left-nav/left-nav.component';
     HomeComponent,
     TemplateComponent,
     RightNavComponent,
-    LeftNavComponent
+    LeftNavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
