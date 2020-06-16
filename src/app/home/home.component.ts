487e86a24fa3537dc43b78574a820546689b9a3d
luluket2000 <luluket2000@yahoo.com>
2020-06-16 14:36:22 -0400

    update pod recording

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,22 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { map, shareReplay } from 'rxjs/operators';
+import { PodcastRepoService } from '../podcast/podcast-repo.service';
+import { IPodcast } from '../podcast/podcast.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {  
-  
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-  .pipe(
-    map(result => result.matches),
-    shareReplay()
-  );
+export class HomeComponent implements OnInit {
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  podcast: IPodcast;
+
+  constructor(private breakpointObserver: BreakpointObserver, private podcastRepo: PodcastRepoService) { }
 
   ngOnInit(): void {
+    this.podcastRepo.getAll().subscribe(result => this.podcast = result[0]);
   }
 }
