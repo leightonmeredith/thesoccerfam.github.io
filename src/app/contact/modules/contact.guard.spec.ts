6cdd03570601411bbe6fdf0a9e7342fdb69f16ac
luluket2000 <luluket2000@yahoo.com>
2020-08-21 17:36:24 -0400

    add contact guard

---
+++ src/app/contact/modules/contact.guard.spec.ts
@@ -0,0 +1,22 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ContactGuard } from './contact.guard';
+
+describe('ContactGuard', () => {
+  let guard: ContactGuard;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    guard = TestBed.inject(ContactGuard);
+  });
+
+  it('should be created', () => {
+    expect(guard).toBeTruthy();
+  });
+
+  describe('canActivate', () => {
+    it('should return true', () => {
+      guard.canActivate(null, null);
+    })
+  })
+});
